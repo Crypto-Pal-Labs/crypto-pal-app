14c5e62240f9eca58b6fe12f42eacdb270eb0397
Leighton <flagshipdevelopments@gmail.com>
2025-08-15 12:15:40 +1200

    config: Added dotenv plugin to babel.config.js for @env resolution

--- babel.config.js
+++ babel.config.js
@@ -1,12 +1,11 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
     plugins: [
-      'react-native-reanimated/plugin', // If already there
-      ['module:react-native-dotenv', { // Add this if missing
-        envName: 'APP_ENV',
-        moduleName: 'react-native-dotenv',
+      // Existing plugins, e.g., 'react-native-reanimated/plugin',
+      ['module:react-native-dotenv', {
+        moduleName: '@env',
         path: '.env',
         safe: false,
         allowUndefined: true,
