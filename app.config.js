ff3e4281eb29e57782102bb29b1821e69b6d336b
Leighton <flagshipdevelopments@gmail.com>
2025-10-08 09:51:28 +1300

    Final env fix for APK - static ENV & secrets creation - Oct07-2025

--- app.config.js
+++ app.config.js
@@ -41,20 +41,7 @@ module.exports = ({ config }) => {
       },
       // Map from process.env (EAS injection) to extra for APK bundling
       COVALENT_KEY: process.env.EXPO_PUBLIC_COVALENT_KEY,
-      TRANSAK_API_KEY: process.env.EXPO_PUBLIC_TRANSAK_API_KEY,
-      ONE_INCH_API_KEY: process.env.EXPO_PUBLIC_ONE_INCH_API_KEY,
-      ETHERSCAN_BASE: process.env.EXPO_PUBLIC_ETHERSCAN_BASE,
-      ETH_RPC_URL: process.env.EXPO_PUBLIC_ETH_RPC_URL,
-      BSC_RPC_URL: process.env.EXPO_PUBLIC_BSC_RPC_URL,
-      BSCSCAN_BASE: process.env.EXPO_PUBLIC_BSCSCAN_BASE,
-      WALLET_CONNECT_PROJECT_ID: process.env.EXPO_PUBLIC_WALLET_CONNECT_PROJECT_ID,
-      ONE_INCH_API_BASE: process.env.EXPO_PUBLIC_ONE_INCH_API_BASE,
-      UNISWAP_ROUTER_ADDRESS: process.env.EXPO_PUBLIC_UNISWAP_ROUTER_ADDRESS,
-      USDC_ADDRESS: process.env.EXPO_PUBLIC_USDC_ADDRESS,
-      WETH_ADDRESS: process.env.EXPO_PUBLIC_WETH_ADDRESS,
-      CONTRACT_ADDRESS: process.env.EXPO_PUBLIC_CONTRACT_ADDRESS,
-      ALCHEMY_KEY: process.env.EXPO_PUBLIC_ALCHEMY_KEY,
-      POLYGON_RPC_URL: process.env.EXPO_PUBLIC_POLYGON_RPC_URL
+      // Map other keys as needed
     }
   };
 };
