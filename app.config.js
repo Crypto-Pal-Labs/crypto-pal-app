c74a741f31ed53e421030ce53a51774681cb9a80
Leighton <flagshipdevelopments@gmail.com>
2025-10-22 16:35:33 +1300

    Android: compile/target SDK 35 (edge-to-edge attr fix); minSdk 24

--- app.config.js
+++ app.config.js
@@ -37,8 +37,8 @@ module.exports = ({ config }) => ({
       'expo-build-properties',
       {
         android: {
-          compileSdkVersion: 34,
-          targetSdkVersion: 34,
+          compileSdkVersion: 35,
+          targetSdkVersion: 35,
           minSdkVersion: 24,
           buildToolsVersion: '34.0.4',
         },
